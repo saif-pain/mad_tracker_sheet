9e474b8b6cece0e4a0bf4a2e76d4906fbad27726
saif-pain <saifalmamun555@gmail.com>
2025-04-11 19:02:49 +0600

    adding 12th task

---
+++ problem_12/lib/main.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Styled Container',
+      theme: ThemeData(primarySwatch: Colors.blue),
+      home: HomeScreen(),
+    );
+  }
+}
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey[200],
+      body: Center(
+        child: Container(
+          padding: EdgeInsets.all(100),
+          decoration: BoxDecoration(
+            color: Colors.green,
+            borderRadius: BorderRadius.circular(16),
+            boxShadow: [
+              BoxShadow(
+                color: Colors.black26,
+                blurRadius: 10,
+                offset: Offset(10, 10),
+              ),
+            ],
+          ),
+          child: Text(
+            'Hello, I am a Container!',
+            style: TextStyle(color: Colors.black, fontSize: 20),
+          ),
+        ),
+      ),
+    );
+  }
+}
